4bfb70cb2ede7ce85c72d03f3034a92980a966c3
Fred <akamaatei@gmail.com>
2020-10-11 23:59:29 +0300

    add last functions needed to start the main function

--- run.py
+++ run.py
@@ -31,3 +31,46 @@ def signin_user(Username, Password):
     """
     confirm_user = Credentials.true_user(Username, Password)
     return confirm_user
+
+
+def create_credentials(App, Username, Password):
+    '''
+    Function to create new credentails
+    '''
+    new_credentials = Credentials(Username, Username, Password)
+    return new_credentials
+
+
+def save_credentials(credentials):
+    '''
+    Function to save contact
+    '''
+    credentials.save_credentials()
+
+
+def del_credentials(credentails):
+    '''
+    Function to delete a contact
+    '''
+    credentails.delete_credentials()
+
+
+def find_credentials(App):
+    '''
+    Function that finds a credential by App and returns the credential
+    '''
+    return Credentials.find_credentials(App)
+
+
+def check_credentials(App):
+    '''
+    Function that check if a credential exists with that app and return a Boolean
+    '''
+    return Credentials.credentials_exist(App)
+
+
+def display_credentials():
+    '''
+    Function that returns all the saved contacts
+    '''
+    return Credentials.display_credentials()
