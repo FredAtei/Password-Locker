f0b7dcda50497ae69feac688ed94ec692e9530f1
Fred <akamaatei@gmail.com>
2020-10-11 23:15:42 +0300

    add functions to the run file

--- run.py
+++ run.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3.6
+from passfile import User
+from passfile import Credentials
+
+
+def create_User(Username, Password):
+    '''
+    Function to create a new contact
+    '''
+    new_User = User(Username, Password)
+    return new_User
+
+
+def save_User(User):
+    '''
+    Function to save contact
+    '''
+    User.save_User()
+
+
+def del_User(User):
+    '''
+    Function to delete a contact
+    '''
+    User.delete_User()
