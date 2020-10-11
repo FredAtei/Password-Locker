c860f6afff8cbadb95ca367e612e5108499dd41a
Fred <akamaatei@gmail.com>
2020-10-11 13:36:50 +0300

    first commit

---
+++ password.py
@@ -0,0 +1,12 @@
+class User:
+    """
+    Class that generates new instances of the User
+    """
+
+    User_list = []
+
+    def __init__(self, Username, Password):
+
+      # docstring removed for simplicity
+        self.Username = Username
+        self.Password = Password
